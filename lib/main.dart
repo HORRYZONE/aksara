8da1378ca5a4815420f3df685a2d2e4a3eb1c369
Elyas Asmad <elyasasmadz@gmail.com>
2023-02-28 23:53:22 +0800

    ✨ Implement GetX state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'dart:io';
 
-import 'package:aksara/app_theme.dart';
+import 'package:aksara/controllers/app_controller.dart';
+import 'package:aksara/utils/app_theme.dart';
+import 'package:aksara/screens/home.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 void main() async {
-  runApp(const MyApp());
+  await GetStorage.init();
+
+  runApp(MyApp());
 
   if (!kIsWeb && Platform.isAndroid) {
     await FlutterDisplayMode.setHighRefreshRate();
@@ -15,66 +21,22 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final AppController c = Get.put(AppController());
 
   @override
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
         builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-      return MaterialApp(
+      return GetMaterialApp(
         title: 'Flutter Demo',
         theme: AksaraTheme(lightDynamic).lightMode(),
         darkTheme: AksaraTheme(darkDynamic).darkMode(),
-        themeMode: ThemeMode.system,
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        themeMode: ThemeMode.light,
+        initialRoute: '/',
+        routes: {'/': (context) => const HomeScreen()},
       );
     });
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
   }
 }
